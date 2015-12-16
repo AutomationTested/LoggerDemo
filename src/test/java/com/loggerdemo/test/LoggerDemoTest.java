3c3554afa844b9e55f2f3096ba2f111741463497
pixaloop <pixaloop@gmail.com>
2015-12-16 10:54:34 -0800

    kept the main package

--- src/test/java/com/loggerdemo/test/LoggerDemoTest.java
+++ src/test/java/com/loggerdemo/test/LoggerDemoTest.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Filename: LoggerDemoTest.java
+ * Author: David N.
+ * Version: Dec 2015
+ * Description: To demonstrate the implementation of basic log4j2 logging within
+ * an automation framework.
+ *******************************************************************************/
 package com.loggerdemo.test;
 
 import org.apache.logging.log4j.LogManager;
@@ -5,34 +12,30 @@ import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 public class LoggerDemoTest {
-
 
     final static Logger log = LogManager.getLogger(LoggerDemoTest.class.getName());
 
+    @Test
+    public static void maintest() throws Exception {
 
-        @Test
-        public static void maintest() throws Exception{
+        log.trace("trace");
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
+        log.fatal("fatal");
 
-		 log.trace("trace");
-		 log.debug("debug");
-		 log.info("info");
-		 log.warn("warn");
-		 log.error("error");
-		 log.fatal("fatal");
+        try {
+            int s;
+            s = 9 / 3;
+            System.out.println("The current value for 's' is now: " + s);
+            Assert.assertEquals(3, s);
 
-            try{
-    			int s;
-    			s = 9/3;
-                System.out.println("The current value for 's' is now: " + s);
-                Assert.assertEquals(3,s);
-
-            }catch (Exception e){
-                log.error("Failed to divide by zero.",e);
-            }
-
-
+        } catch (Exception e) {
+            log.error("Failed to divide by zero.", e);
         }
+
+    }
 }
 
